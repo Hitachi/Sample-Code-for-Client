bf77ab88221b51db9fe26c9360c533cbe4e0ecc2
Takashi Mogi <takashi.mogi.ep@hitachi.com>
2021-08-26 11:42:52 +0900

    fix-log-format

--- src/main/java/sample/clientapp/ClientAppController.java
+++ src/main/java/sample/clientapp/ClientAppController.java
@@ -105,7 +105,7 @@ public class ClientAppController {
         if (req.hasBody()) {
             message.put("body", req.getBody());
         }
-        logger.debug("ReqeustType=\"" + msg + "\" RequestInfo=" + writeJsonString(req, false));
+        logger.debug("ReqeustType=\"" + msg + "\" RequestInfo=" + writeJsonString(message, false));
         return;
     }
 
